8ba8521a243b23f1e974fdb11268fdb76fe40f39
Your Name <you@example.com>
2025-10-11 17:45:05 +0700

    change UI design

--- main.py
+++ main.py
@@ -7,36 +7,105 @@ class SkelGenDialog(QtWidgets.QDialog):
 		super().__init__(parent)
 
 		self.setWindowTitle('Skeleton Generate')
-		self.resize(500,300)
+		self.resize(100,100)
 
 		self.mainLayout = QtWidgets.QVBoxLayout()
 		self.setLayout(self.mainLayout)
-		self.setStyleSheet( 'background-color: qlineargradient(x1:1, y1:0, x2:1, y2:01, stop:0 #0033FF, stop:1 #FFCCF2)')
+		self.setStyleSheet( '''
+			QDialog{
+				background-color: qlineargradient(x1:1, y1:0, x2:1, y2:01, stop:0 #0033FF, stop:1 #FFCCF2)
+			}
 
-
-		#self.descriptionLabel = QtWidgets.QLabel('foo osdofdp')
+			'''
+		)
 
 
 		self.buttonLayout = QtWidgets.QHBoxLayout()
 		self.mainLayout.addLayout(self.buttonLayout)
 		self.createGuidelineButton = QtWidgets.QPushButton('Create Guideline')
+		self.buttonLayout.addWidget(self.createGuidelineButton)					
+		self.createGuidelineButton.setStyleSheet('''
+			QPushButton{
+				background-color: #977DFF;
+
+			}
+			QPushButton:hover {
+				background-color: #FFCCF2;
+			}
+			QPushButton:pressed {
+				background-color: #0600AB;
+			}
+			'''
+		)
+
+		self.buttonLayout = QtWidgets.QHBoxLayout()
+		self.mainLayout.addLayout(self.buttonLayout)
 		self.clearGuidelineButton = QtWidgets.QPushButton('Clear Guideline')
-		self.buttonLayout.addWidget(self.createGuidelineButton)
 		self.buttonLayout.addWidget(self.clearGuidelineButton)
+		self.clearGuidelineButton.setStyleSheet('''
+			QPushButton{
+				background-color: #977DFF;
+
+			}
+			QPushButton:hover {
+				background-color: #FFCCF2;
+			}
+			QPushButton:pressed {
+				background-color: #0600AB;
+			}
+			'''
+		)
+
+		self.descriptionLabel = QtWidgets.QLabel('Before using Generate Joint')
+		self.mainLayout.addWidget(self.descriptionLabel)
+		# self.descriptionLabel.setStyleSheet("padding-top:15px;")
+
+		self.descriptionLabel = QtWidgets.QLabel('Make sure that you already adjust joint in the right position')
+		self.mainLayout.addWidget(self.descriptionLabel)
 
 		self.buttonLayout = QtWidgets.QHBoxLayout()
 		self.mainLayout.addLayout(self.buttonLayout)
 		self.generateJointButton = QtWidgets.QPushButton('Generate Joint')
-		self.mirrorJointButton = QtWidgets.QPushButton('Mirror Joint')
 		self.buttonLayout.addWidget(self.generateJointButton)
-		self.buttonLayout.addWidget(self.mirrorJointButton)
+		self.generateJointButton.setStyleSheet('''
+			QPushButton{
+				background-color: #977DFF;
+
+			}
+			QPushButton:hover {
+				background-color: #FFCCF2;
+			}
+			QPushButton:pressed {
+				background-color: #0600AB;
+			}
+			'''
+		)
 
 		self.buttonLayout = QtWidgets.QHBoxLayout()
 		self.mainLayout.addLayout(self.buttonLayout)
-		self.applyButton = QtWidgets.QPushButton('APPLY')
-		self.cancelButton = QtWidgets.QPushButton('CANCEL')
-		self.buttonLayout.addWidget(self.applyButton)
-		self.buttonLayout.addWidget(self.cancelButton)
+		self.mirrorJointButton = QtWidgets.QPushButton('Mirror Joint')
+		self.buttonLayout.addWidget(self.mirrorJointButton)
+		self.mirrorJointButton.setStyleSheet('''
+			QPushButton{
+				background-color: #977DFF;
+
+			}
+			QPushButton:hover {
+				background-color: #FFCCF2;
+			}
+			QPushButton:pressed {
+				background-color: #0600AB;
+			}
+			'''
+		)
+		# self.buttonLayout = QtWidgets.QHBoxLayout()
+		# self.mainLayout.addLayout(self.buttonLayout)
+		# self.applyButton = QtWidgets.QPushButton('APPLY')
+		# self.cancelButton = QtWidgets.QPushButton('CANCEL')
+		# self.buttonLayout.addWidget(self.applyButton)
+		# self.buttonLayout.addWidget(self.cancelButton)
+
+		self.mainLayout.addStretch()
 
 def run():
 	global ui
